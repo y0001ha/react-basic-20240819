fb2e227d35e47bb8876a76e3fcab165ffc0bb667
y0001ha <yyy96110@naver.com>
2024-08-20 11:56:31 +0900

    20240820 11:50

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Component, { FunctionComponent } from './component/Component';
 import './App.css';
 import CurlyBraces from './component/CurlyBraces';
 import Properties from './component_manage/Properties';
+import ConditionalRender from './component_manage/ConditionalRender';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/* <Component /> */}
       {/* <FunctionComponent /> */}
       {/* <CurlyBraces /> */}
-      <Properties />
+      {/* <Properties /> */}
+      <ConditionalRender />
     </>
   );
 }
--- src/component_manage/example/Example1.tsx
+++ src/component_manage/example/Example1.tsx
@@ -43,76 +43,34 @@ function Profile({ name, imageId, profession, awardsCount, awards, discovered }:
                     </li>
                 </ul>
             </section>
-
-
     );
-
 }
 
 export default function Gallery() {
+
+    const profile1 = {
+        name:'Maria Skłodowska-Curie', 
+        imageId:'szV5sdG',
+        profession:'physicist and chemist',
+        awardsCount: 4,
+        awards:'Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal',
+        discovered:'polonium (chemical element)'
+    }
+
+    const profile2 = {
+        name:'Katsuko Saruhashi', 
+        imageId:'YfeOqp2',
+        profession:'geochemist',
+        awardsCount: 2,
+        awards:'Miyake Prize for geochemistry, Tanaka Prize',
+        discovered:'a method for measuring carbon dioxide in seawater'
+    }
+
     return (
         <div>
             <h1>Notable Scientists</h1>
-
-
-            <Profile 
-                name="Maria Skłodowska-Curie" 
-                imageId='szV5sdG'
-                profession='physicist and chemist'
-                awardsCount={4}
-                awards='Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal'
-                discovered='polonium (chemical element)'
-            />
-
-
-            <section className="profile">
-                <h2>Maria Skłodowska-Curie</h2>
-                <img
-                    className="avatar"
-                    src={getImageUrl('szV5sdG')}
-                    alt="Maria Skłodowska-Curie"
-                    width={70}
-                    height={70}
-                />
-                <ul>
-                    <li>
-                        <b>Profession: </b>
-                        physicist and chemist
-                    </li>
-                    <li>
-                        <b>Awards: 4 </b>
-                        (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-                    </li>
-                    <li>
-                        <b>Discovered: </b>
-                        polonium (chemical element)
-                    </li>
-                </ul>
-            </section>
-            <section className="profile">
-                <h2>Katsuko Saruhashi</h2>
-                <img
-                    className="avatar"
-                    src={getImageUrl('YfeOqp2')}
-                    alt="Katsuko Saruhashi"
-                    width={70}
-                    height={70}
-                />
-                <ul>
-                    <li>
-                        <b>Profession: </b>
-                        geochemist
-                    </li>
-                    <li>
-                        <b>Awards: 2 </b>
-                        (Miyake Prize for geochemistry, Tanaka Prize)
-                    </li>
-                    <li>
-                        <b>Discovered: </b>
-                        a method for measuring carbon dioxide in seawater
-                    </li>
-                </ul>
-            </section>
+            <Profile {...profile1} />
+            <Profile {...profile2} />
         </div>
     );
 }
