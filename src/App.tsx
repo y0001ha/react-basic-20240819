a5f7b9c1867f5ef7b15f040490b37fa0c1eaf8f5
y0001ha <yyy96110@naver.com>
2024-08-22 10:52:01 +0900

    20240822 10:50

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Properties from './component_manage/Properties';
 import ConditionalRender from './component_manage/ConditionalRender';
 import Example2 from './component_manage/example/Example2';
 import ListRender from './component_manage/ListRender';
+import EventComponent from './interaction/EventComponent';
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
       {/* <Properties /> */}
       {/* <ConditionalRender /> */}
       {/* <Example2 /> */}
-      <ListRender />
+      {/* <ListRender /> */}
+      <EventComponent />
     </>
   );
 }
