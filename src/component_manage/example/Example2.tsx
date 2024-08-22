474a12d6cb6fb7ca9c10419513dfb1374d5b79b1
y0001ha <yyy96110@naver.com>
2024-08-22 09:06:25 +0900

    20240822 09:00

--- src/component_manage/example/Example2.tsx
+++ src/component_manage/example/Example2.tsx
@@ -1,33 +1,130 @@
 import React from 'react'
 import './Example2.css';
 
-export default function Example2() {
+function TableHeader() {
+
     return (
-        <div className='table'>
+        <div className='th'>
+            <div className='td'>사번</div>
+            <div className='td'>이름</div>
+            <div className='td'>출근상태</div>
+        </div>
+    );
+
+}
+
+interface TableRowProps {
+    employeeNumber: string;
+    employeeName: string;
+    workState: boolean;
+}
+
+function TableRow({ employeeNumber, employeeName, workState }: TableRowProps) {
+
+    const color = workState ? 'blue' : 'red';
+
+    return (
+        <div className='tr'>
+            <div className='td'>{employeeNumber}</div>
+            <div className='td'>{employeeName}</div>
+            <div className='td' style={{ color }}>●</div>
+        </div>
+    );
+
+}
+
+interface TableDataTrProps {
+    type: 'tr';
+    employeeNumber: string;
+    employeeName: string;
+    workState: boolean;
+}
+
+interface TableDataThProps {
+    type: 'th';
+}
+
+function TableData(props: TableDataTrProps | TableDataThProps) {
+
+    const { type } = props;
+
+    if (type === 'th') {
+        return (
             <div className='th'>
                 <div className='td'>사번</div>
                 <div className='td'>이름</div>
                 <div className='td'>출근상태</div>
             </div>
+        );
+    }
 
-            <div className='tr'>
-                <div className='td'>202401</div>
-                <div className='td'>홍길동</div>
-                <div className='td' style={{color: 'red'}}>●</div>
-            </div>
+    const { employeeName, employeeNumber, workState } = props;
+    const color = workState ? 'blue' : 'red';
 
-            <div className='tr'>
-                <div className='td'>202402</div>
-                <div className='td'>이영희</div>
-                <div className='td' style={{color: 'blue'}}>●</div>
-            </div>
+    return (
+        <div className='tr'>
+            <div className='td'>{employeeNumber}</div>
+            <div className='td'>{employeeName}</div>
+            <div className='td' style={{ color }}>●</div>
+        </div>
+    );
 
-            <div className='tr'>
-                <div className='td'>202403</div>
-                <div className='td'>김철수</div>
-                <div className='td' style={{color: 'blue'}}>●</div>
-            </div>
+}
 
+export default function Example2() {
+
+    const employee1 = {
+        employeeNumber: '202401',
+        employeeName: '홍길동',
+        workState: false
+    };
+
+    const employee2 = {
+        employeeNumber: '202402',
+        employeeName: '이영희',
+        workState: true
+    };
+
+    const employee3 = {
+        employeeNumber: '202403',
+        employeeName: '김철수',
+        workState: true
+    };
+
+    return (
+        <div className='table'>
+            {/* <TableHeader />
+            <TableRow { ...employee1 } />
+            <TableRow { ...employee2 } />
+            <TableRow { ...employee3 } /> */}
+
+            {/* <TableData type='th' />
+            <TableData type='tr' {...employee1} />
+            <TableData type='tr' {...employee2} />
+            <TableData type='tr' {...employee3} /> */}
+
+            <TableHeader />
+            <TableRow { ...employees[0] } />
+            <TableRow { ...employees[1] } />
+            <TableRow { ...employees[2] } />
         </div>
     )
 }
+
+const employees = [
+    {
+        employeeNumber: '202401',
+        employeeName: '홍길동',
+        workState: false
+    },
+    {
+        employeeNumber: '202402',
+        employeeName: '이영희',
+        workState: true
+    },
+    {
+        employeeNumber: '202403',
+        employeeName: '김철수',
+        workState: true
+    }
+];
