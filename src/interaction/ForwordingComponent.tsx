f80b41683b800c822587a87575c5e5ef9e6c5547
y0001ha <yyy96110@naver.com>
2024-08-23 10:51:01 +0900

    20240823 10:50

---
+++ src/interaction/ForwordingComponent.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+// 컴포넌트로 상태 전달 : 
+// - 부모 요소에서 자식요소로 상태를 전달할 수 있음
+// - 자식 요소의 속성으로 전달
+export default function ForwordingComponent() {
+
+
+    return (
+        <div>
+            <Child1 />
+            <Child2 />
+            <Child3 />
+        </div>
+    )
+}
+
+
+function Child1 () {
+    return (
+        <div style={{ height: '200px', backgroundColor: 'red' }}></div>
+    )
+}
+
+function Child2 () {
+    return (
+        <div style={{ height: '200px', backgroundColor: 'green' }}></div>
+    )
+}
+
+function Child3 () {
+    return (
+        <div style={{ height: '200px', backgroundColor: 'blue' }}></div>
+    )
+}
