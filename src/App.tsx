59e3cb69397e032eb199a8c6fcbe5700c9b7baa9
y0001ha <yyy96110@naver.com>
2024-08-30 10:50:39 +0900

    20240830 10:50

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import { Outlet, Route, Routes, useLocation } from 'react-router';
 import QueryString from './router/QueryString';
 import PathVariable from './router/PathVariable';
 import PathMove from './router/PathMove';
+import Zustand from './zustand/Zustand';
 
 // react-router 패키지:
 // - react의 SPA(Single Page Application)에서 라우팅을 구현하기 위한 라이브러리
@@ -70,6 +71,7 @@ function App() {
         <Route path='path-variable/:name' element={<PathVariable />} />
         <Route path='path-move' element={<PathMove />} />
       </Route>
+      <Route path='/zustand' element={<Zustand />} />
       <Route path='*' element={<h1>404!!!</h1>} />
       
       {/* <Component /> */}
