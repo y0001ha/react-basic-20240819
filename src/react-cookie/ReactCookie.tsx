58a8a4b727e285be80ed16460b0ac9c58364eabc
y0001ha <yyy96110@naver.com>
2024-09-03 10:54:15 +0900

    20240903 10:50

--- src/react-cookie/ReactCookie.tsx
+++ src/react-cookie/ReactCookie.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import exp from 'constants';
+import React, { ChangeEvent, useState } from 'react'
+import { useCookies } from 'react-cookie';
 
 // react-cookie 패키지:
 // - react에서 쿠키를 손쉽게 관리할 수 있도록 도움을 주는 패키지
@@ -13,16 +15,60 @@ import React, { useState } from 'react'
 
 export default function ReactCookie() {
 
+    // useCookies : 리액트에서 쿠키 객체, 쿠키 변경 함수, 쿠키 제거 함수를 배열로 반환하는 react-cookie의 훅 함수
+    const [cookies, setCookie, removeCookie] = useCookies();
+
     const [name, setName] = useState<string>('');
     const [value, setValue] = useState<string>('');
 
+    const onNameChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        setName(value);
+    };
+
+    const onValueChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        setValue(value);
+    };
+
+    const onAddClickHandler = () => {
+        // setCookie 함수를 사용하여 쿠키를 추가할 수 있음
+        // 첫번째 매개변수 : 쿠키 이름
+        // 두번째 매개변수 : 쿠키 값
+        // 세번째 매개변수(선택) : 옵션 지정
+        // setCookie(name, value);
+
+        // 옵션
+        // - path : 해당 쿠키가 적용될 경로
+        // - expires : 만료시간
+        // setCookie(name, value, { path: '/zustand'});
+        const now = new Date();
+        const expires = new Date(now.setSeconds(now.getSeconds() +10));
+        setCookie(name, value, { path: '/' });
+    };
+
+    const onShowClickHandler = () => {
+        // cookies 객체에 브라우저가 가지고 있는 쿠키 정보가 담겨있음
+        // alert(JSON.stringify(cookies));
+        // alert(cookies.first);
+        alert(cookies[name]);
+    };
+
+    const onRemoveClickHandler = () => {
+        // removeCookie 함수를 사용하여 쿠키 제거 가능
+        // 첫번째 매개변수 : 쿠키 이름
+        // 두번째 매개변수 : 옵션
+        // removeCookie(name);
+        removeCookie(name, { path: '/zustand' });
+    };
+
     return (
         <div>
-            <input />
-            <input />
-            <button>쿠키 추가</button>
-            <button>쿠키 확인</button>
-            <button>쿠키 제거</button>
+            <input value={name} onChange={onNameChangeHandler}/>
+            <input value={value} onChange={onValueChangeHandler}/>
+            <button onClick={onAddClickHandler}>쿠키 추가</button>
+            <button onClick={onShowClickHandler}>쿠키 확인</button>
+            <button onClick={onRemoveClickHandler}>쿠키 제거</button>
         </div>
     )
 }
